f3dab376a8ed3edc77b28ef5aa79692f82aa51be
mguibtech <mguibtech@gmail.com>
2023-09-24 15:08:17 -0400

    feat: Adding Spacer component

--- App.tsx
+++ App.tsx
@@ -3,12 +3,16 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 import { defaultTheme } from './src/common/constants/styles/theme/defaultTheme';
 import Text from './src/common/components/Text';
+import Spacer from './src/common/components/Spacer';
 
 export default function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <View style={styles.container}>
-        <Text size={50} color="secondary">Testando teste</Text>
+        <Text size={20} color="secondary">Testando teste</Text>
+        <Spacer height={20}/>
+        <Text size={20} color="secondary">Testando teste</Text>
+
         <StatusBar style="auto" />
       </View>
     </ThemeProvider>
