9c4a1850c3c03eb7c1eb5088613dd533b0ed54dc
mguibtech <mguibtech@gmail.com>
2023-09-24 14:55:41 -0400

    feat: Adding new text componnet

--- App.tsx
+++ App.tsx
@@ -1,13 +1,14 @@
 import { ThemeProvider } from 'styled-components/native';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { defaultTheme } from './src/common/constants/styles/theme/defaultTheme';
+import Text from './src/common/components/Text';
 
 export default function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <View style={styles.container}>
-        <Text>Initial project</Text>
+        <Text size={50} color="secondary">Testando teste</Text>
         <StatusBar style="auto" />
       </View>
     </ThemeProvider>
---
+++ src/common/components/Text/styles.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components/native";
+import { Props } from "./types";
+
+export const Container = styled.Text<Partial<Props>>`
+    color: ${({theme, color}) => theme.colors[color || 'onSecondary']};
+    font-size: ${({size}) => size || 14}px;
+
+
+
+`
